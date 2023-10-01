7b9e9348c3c0f2ecb2db9beb8ce00cf43e052a4b
salim <Muhammed.Salim@windstream.com>
2023-10-01 12:16:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App" style={{ backgroundColor: "green", flex: 1, height: 100, width: 100 }}>
+
+    </div>
+  );
+}
+
+export default App;
